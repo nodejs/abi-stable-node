6d8e615d169fd853285a5bfec30e713189fe3805
Ian Halliday <ianhall@microsoft.com>
2016-06-30 02:14:37 -0700

    Harden type safety of opaque types
    Use undefined structs in typedefs instead of void* to gain compile time
    type safety.  void*'s permissive casting carries through to typedef'ed
    aliases of it, so it was easy to accidently pass a napi_value where a
    napi_env was expected, for example.

--- src/node_jsvmapi_types.h
+++ src/node_jsvmapi_types.h
@@ -2,11 +2,12 @@
 #define NODE_JAVASCRIPT_API_TYPES_H_
 
 // JSVM API types are all opaque pointers for ABI stability
-typedef void* napi_env;
-typedef void* napi_value;
-typedef void* napi_persistent;
-typedef napi_value napi_propertyname;
-typedef const void* napi_func_cb_info;
+// typedef undefined structs instead of void* for compile time type safety
+typedef struct napi_env__ *napi_env;
+typedef struct napi_value__ *napi_value;
+typedef struct napi_persistent__ *napi_persistent;
+typedef struct napi_propertyname__ *napi_propertyname;
+typedef const struct napi_func_cb_info__ *napi_func_cb_info;
 typedef void (*napi_callback)(napi_env, napi_func_cb_info);
 typedef void napi_destruct(void*);
 
