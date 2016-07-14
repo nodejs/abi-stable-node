0d92cf596289f5b4603d764e3e4ae6626aca76e0
Ian Halliday <ianhall@microsoft.com>
2016-07-13 17:56:54 -0700

    Rename napi_throw_error to napi_throw
    napi_throw_error is misleading since you can legitimately throw any
    napi_value (any javascript value).  So renamed to napi_throw to clarify.
    This also leaves room for a napi_throw_error which would be a helper
    that creates a new error from a string and throws it for you, to
    collapse the otherwise three calls that would be needed into one.
    
    Also renamed a bunch of parameters named napi_env to just env for
    clarity.

--- test/addons-abi/test_string/test_string.cc
+++ test/addons-abi/test_string/test_string.cc
@@ -1,40 +1,40 @@
 #include <node_jsvmapi.h>
 
-void Test(napi_env napi_env, napi_func_cb_info info) {
-  if (napi_get_cb_args_length(napi_env, info) < 1) {
-    napi_throw_error(
-          napi_env,
+void Test(napi_env env, napi_func_cb_info info) {
+  if (napi_get_cb_args_length(env, info) < 1) {
+    napi_throw(
+          env,
           napi_create_type_error(
-              napi_env,
-              napi_create_string(napi_env, "Wrong number of arguments")));
+              env,
+              napi_create_string(env, "Wrong number of arguments")));
     return;
   }
 
   napi_value args[1];
-  napi_get_cb_args(napi_env, info, args, 1);
+  napi_get_cb_args(env, info, args, 1);
 
-  if (napi_get_type_of_value(napi_env, args[0]) != napi_string) {
-    napi_throw_error(
-        napi_env,
+  if (napi_get_type_of_value(env, args[0]) != napi_string) {
+    napi_throw(
+        env,
         napi_create_type_error(
-             napi_env,
-             napi_create_string(napi_env, "Wrong type of argments. Expects a string.")));
+             env,
+             napi_create_string(env, "Wrong type of argments. Expects a string.")));
     return;
   }
 
   char buffer[128];
   int buffer_size = 128;
-  bool success = napi_get_string_from_value(napi_env, args[0], buffer, buffer_size);
+  bool success = napi_get_string_from_value(env, args[0], buffer, buffer_size);
   if (success) {
-    napi_value output = napi_create_string(napi_env, buffer);
-    napi_set_return_value(napi_env, info, output);
+    napi_value output = napi_create_string(env, buffer);
+    napi_set_return_value(env, info, output);
   }
 }
 
-void Init(napi_env napi_env, napi_value exports, napi_value module) {
-  napi_set_property(napi_env, exports,
-                    napi_property_name(napi_env, "Test"),
-                    napi_create_function(napi_env, Test));
+void Init(napi_env env, napi_value exports, napi_value module) {
+  napi_set_property(env, exports,
+                    napi_property_name(env, "Test"),
+                    napi_create_function(env, Test));
 }
 
 NODE_MODULE_ABI(addon, Init)
