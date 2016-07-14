241a4915b28aae46a773b389e12147ffd8f56bba
Ian Halliday <ianhall@microsoft.com>
2016-07-13 17:56:54 -0700

    Rename napi_throw_error to napi_throw
    napi_throw_error is misleading since you can legitimately throw any
    napi_value (any javascript value).  So renamed to napi_throw to clarify.
    This also leaves room for a napi_throw_error which would be a helper
    that creates a new error from a string and throws it for you, to
    collapse the otherwise three calls that would be needed into one.
    
    Also renamed a bunch of parameters named napi_env to just env for
    clarity.

--- test/addons-abi/test_object/test_object.cc
+++ test/addons-abi/test_object/test_object.cc
@@ -1,32 +1,32 @@
 #include <node_jsvmapi.h>
 
-void Test(napi_env napi_env, napi_func_cb_info info) {
-  if (napi_get_cb_args_length(napi_env, info) < 2) {
-    napi_throw_error(
-          napi_env,
+void Test(napi_env env, napi_func_cb_info info) {
+  if (napi_get_cb_args_length(env, info) < 2) {
+    napi_throw(
+          env,
           napi_create_type_error(
-              napi_env,
-              napi_create_string(napi_env, "Wrong number of arguments")));
+              env,
+              napi_create_string(env, "Wrong number of arguments")));
     return;
   }
 
   napi_value args[2];
-  napi_get_cb_args(napi_env, info, args, 2);
+  napi_get_cb_args(env, info, args, 2);
 
-  if (napi_get_type_of_value(napi_env, args[0]) != napi_object) {
-    napi_throw_error(
-        napi_env,
+  if (napi_get_type_of_value(env, args[0]) != napi_object) {
+    napi_throw(
+        env,
         napi_create_type_error(
-             napi_env,
-             napi_create_string(napi_env, "Wrong type of argments. Expects an object as first argument.")));
+             env,
+             napi_create_string(env, "Wrong type of argments. Expects an object as first argument.")));
     return;
   }
-  if (napi_get_type_of_value(napi_env, args[1]) != napi_string) {
-    napi_throw_error(
-        napi_env,
+  if (napi_get_type_of_value(env, args[1]) != napi_string) {
+    napi_throw(
+        env,
         napi_create_type_error(
-             napi_env,
-             napi_create_string(napi_env, "Wrong type of argments. Expects a string as second argument.")));
+             env,
+             napi_create_string(env, "Wrong type of argments. Expects a string as second argument.")));
     return;
   }
 
@@ -41,10 +41,10 @@ void Test(napi_env napi_env, napi_func_cb_info info) {
   }
 }
 
-void Init(napi_env napi_env, napi_value exports, napi_value module) {
-  napi_set_property(napi_env, exports,
-                    napi_property_name(napi_env, "Test"),
-                    napi_create_function(napi_env, Test));
+void Init(napi_env env, napi_value exports, napi_value module) {
+  napi_set_property(env, exports,
+                    napi_property_name(env, "Test"),
+                    napi_create_function(env, Test));
 }
 
 NODE_MODULE_ABI(addon, Init)
