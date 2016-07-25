5dcd6cf0d27091476c54f28dba88bff9c5a8bd33
Sampson Gao <sampson.gao7@gmail.com>
2016-07-25 16:40:43 -0400

    Modify tests for string

--- test/addons-abi/test_string/test.js
+++ test/addons-abi/test_string/test.js
@@ -5,8 +5,8 @@ var assert = require('assert');
 // testing api calls for string
 var test_string = require('./build/Release/test_string');
 
-var strings = ['hello world', 'a', '?!@#$%^&*()_+-=\[]{}/.,<>', '\u{2003}\u{2101}\u{2001}'];
 
-strings.forEach(function(element, index) {
-  assert.equal(element, test_string.Test(element));
-});
+assert(test_string.Test('hello world'), 'hello world');
+assert(test_string.Test('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789');
+assert(test_string.Test('?!@#$%^&*()_+-=\[]{}/.,<>'), '?!@#$%^&*()_+-=\[]{}/.,<>');
+assert(test_string.Test('\u{2003}\u{2101}\u{2001}'), '\u{2003}\u{2101}\u{2001}');
