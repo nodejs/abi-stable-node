6e38a233881cf0b7683b7c8f3138ea4573ceb82b
Sampson Gao <sampson.gao7@gmail.com>
2016-07-25 11:22:03 -0400

    modify tests for function

--- test/addons-abi/test_function/test.js
+++ test/addons-abi/test_function/test.js
@@ -5,13 +5,23 @@ var assert = require('assert');
 // testing api calls for function
 var test_function = require('./build/Release/test_function');
 
-var functions = [
-  function () { return 1; },
-  function () { return 2; }
-];
 
-functions.forEach(function(element, index) {
-  var expected = element();
-  var output = test_function.Test(element);
-  assert.equal(output, expected);
-});
+function func1 () {
+  return 1;
+}
+assert.equal(test_function.Test(func1), 1);
+
+function func2 () {
+  console.log('hello world!');
+}
+assert.equal(test_function.Test(func2), null);
+
+function func3 (input) {
+  return input + 1;
+}
+assert.equal(test_function.Test(func3, 1), 2); 
+
+function func4 (input) {
+  return func3 (input);
+}
+assert.equal(test_function.Test(func4, 1), 2);
