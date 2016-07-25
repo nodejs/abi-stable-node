a0126dd2f666e8e0e60db626cf385db8a04521e9
Sampson Gao <sampson.gao7@gmail.com>
2016-07-25 10:26:57 -0400

    Add tests for object napi_get_propertynames

--- test/addons-abi/test_object/test.js
+++ test/addons-abi/test_object/test.js
@@ -16,12 +16,29 @@ var object = {
   }
 };
 
-assert.equal(test_object.Test(object, 'hello'), 'world');
-assert.deepEqual(test_object.Test(object, 'array'), [1, 94, 'str', 12.321, { test: 'obj in arr' }]);
-assert.deepEqual(test_object.Test(object, 'newObject'), { test: 'obj in obj' });
+assert.equal(test_object.Get(object, 'hello'), 'world');
+assert.deepEqual(test_object.Get(object, 'array'), [1, 94, 'str', 12.321, { test: 'obj in arr' }]);
+assert.deepEqual(test_object.Get(object, 'newObject'), { test: 'obj in obj' });
 
-
+assert(test_object.Has(object, 'hello'));
+assert(test_object.Has(object, 'array'));
+assert(test_object.Has(object, 'newObject'));
 
 var newObject = test_object.New();
+assert(test_object.Has(newObject, 'test_number'));
 assert.equal(newObject.test_number, 987654321);
 assert.equal(newObject.test_string, "test string");
+
+// test_object.Inflate increases all properties by 1
+var cube = {
+  x: 10,
+  y: 10,
+  z: 10
+};
+
+assert.deepEqual(test_object.Inflate(cube), {x: 11, y: 11, z: 11});
+assert.deepEqual(test_object.Inflate(cube), {x: 12, y: 12, z: 12});
+assert.deepEqual(test_object.Inflate(cube), {x: 13, y: 13, z: 13});
+cube.t = 13;
+assert.deepEqual(test_object.Inflate(cube), {x: 14, y: 14, z: 14, t: 14});
+
