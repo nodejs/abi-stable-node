62884393bfc6c9a08e728b9a1aaa909a41baaccd
Sampson Gao <sampson.gao7@gmail.com>
2016-07-26 13:21:24 -0400

    Add tests for string length

--- test/addons-abi/test_string/test.js
+++ test/addons-abi/test_string/test.js
@@ -5,8 +5,22 @@ var assert = require('assert');
 // testing api calls for string
 var test_string = require('./build/Release/test_string');
 
+var str1 = 'hello world';
+assert.equal(test_string.Copy(str1), str1);
+assert.equal(test_string.Length(str1), 11);
+assert.equal(test_string.Utf8Length(str1), 11);
 
-assert(test_string.Test('hello world'), 'hello world');
-assert(test_string.Test('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789');
-assert(test_string.Test('?!@#$%^&*()_+-=\[]{}/.,<>'), '?!@#$%^&*()_+-=\[]{}/.,<>');
-assert(test_string.Test('\u{2003}\u{2101}\u{2001}'), '\u{2003}\u{2101}\u{2001}');
+var str2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+assert.equal(test_string.Copy(str2), str2);
+assert.equal(test_string.Length(str2), 62);
+assert.equal(test_string.Utf8Length(str2), 62);
+
+var str3 = '?!@#$%^&*()_+-=[]{}/.,<>\'\"\\';
+assert.equal(test_string.Copy(str3), str3);
+assert.equal(test_string.Length(str3), 27);
+assert.equal(test_string.Utf8Length(str3), 27);
+
+var str4 = '\u{2003}\u{2101}\u{2001}';
+assert.equal(test_string.Copy(str4), str4);
+assert.equal(test_string.Length(str4), 3);
+assert.equal(test_string.Utf8Length(str4), 9);
--- test/addons-abi/test_string/test_string.cc
+++ test/addons-abi/test_string/test_string.cc
@@ -1,6 +1,6 @@
 #include <node_jsvmapi.h>
 
-void Test(napi_env env, napi_func_cb_info info) {
+void Copy(napi_env env, napi_func_cb_info info) {
   if (napi_get_cb_args_length(env, info) < 1) {
     napi_throw_type_error(env, "Wrong number of arguments");
     return;
@@ -25,10 +25,54 @@ void Test(napi_env env, napi_func_cb_info info) {
   }
 }
 
+void Length(napi_env env, napi_func_cb_info info) {
+  if (napi_get_cb_args_length(env, info) < 1) {
+    napi_throw_type_error(env, "Wrong number of arguments");
+    return;
+  }
+
+  napi_value args[1];
+  napi_get_cb_args(env, info, args, 1);
+
+  if (napi_get_type_of_value(env, args[0]) != napi_string) {
+    napi_throw_type_error(env, "Wrong type of argments. Expects a string.");
+    return;
+  }
+  
+  int length = napi_get_string_length(env, args[0]);
+  napi_value output = napi_create_number(env, length);
+  napi_set_return_value(env, info, output);
+}
+
+void Utf8Length(napi_env env, napi_func_cb_info info) {
+  if (napi_get_cb_args_length(env, info) < 1) {
+    napi_throw_type_error(env, "Wrong number of arguments");
+    return;
+  }
+
+  napi_value args[1];
+  napi_get_cb_args(env, info, args, 1);
+
+  if (napi_get_type_of_value(env, args[0]) != napi_string) {
+    napi_throw_type_error(env, "Wrong type of argments. Expects a string.");
+    return;
+  }
+  
+  int length = napi_get_string_utf8_length(env, args[0]);
+  napi_value output = napi_create_number(env, length);
+  napi_set_return_value(env, info, output);
+}
+
 void Init(napi_env env, napi_value exports, napi_value module) {
   napi_set_property(env, exports,
-                    napi_property_name(env, "Test"),
-                    napi_create_function(env, Test));
+                    napi_property_name(env, "Copy"),
+                    napi_create_function(env, Copy));
+  napi_set_property(env, exports,
+                    napi_property_name(env, "Length"),
+                    napi_create_function(env, Length));
+  napi_set_property(env, exports,
+                    napi_property_name(env, "Utf8Length"),
+                    napi_create_function(env, Utf8Length));
 }
 
 NODE_MODULE_ABI(addon, Init)
