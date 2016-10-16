6bf9f293780120e5dbac66ec2345461eca923f35
Gabriel Schulhof <gabriel.schulhof@intel.com>
2016-10-16 10:15:19 +0300

    tools: Distribute node_jsvmapi.h header
    This is needed for building outside projects against the ABI stable API

--- tools/install.py
+++ tools/install.py
@@ -133,6 +133,7 @@ def headers(action):
     'common.gypi',
     'config.gypi',
     'src/node.h',
+    'src/node_jsvmapi.h',
     'src/node_buffer.h',
     'src/node_internals.h',
     'src/node_object_wrap.h',
