bbc99428a93dbecb49a7d37894d33d3fd881e1aa
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-01-16 13:07:44 +0200

    Tests: Remove trycatch object from exception test

--- test/addons-abi/test_exception/test.js
+++ test/addons-abi/test_exception/test.js
@@ -3,17 +3,18 @@
 var test_exception = require( "./build/Release/test_exception" );
 var assert = require( "assert" );
 var theError = new Error( "Some error" );
-
-var returnedError = test_exception.returnException( function() {
+var throwTheError = function() {
 	throw theError;
-} );
+};
+
+// Test that the native side successfully captures the exception
+var returnedError = test_exception.returnException( throwTheError );
 assert.strictEqual( theError, returnedError,
 	"Returned error is strictly equal to the thrown error" );
 
+// Test that the native side passes the exception through
 try {
-	test_exception.allowException( function() {
-		throw theError;
-	} );
+	test_exception.allowException( throwTheError );
 } catch ( anError ) {
 	assert.strictEqual( anError, theError,
 		"Thrown error was allowed to pass through unhindered" );
--- test/addons-abi/test_exception/test_exception.cc
+++ test/addons-abi/test_exception/test_exception.cc
@@ -2,36 +2,26 @@
 #include <node_api_helpers.h>
 
 NAPI_METHOD(returnException) {
-	napi_trycatch tryCatch;
 	napi_value returnValue;
 	napi_value jsFunction;
 
 	napi_get_cb_args(env, info, &jsFunction, 1);
-
-	napi_trycatch_new(env, &tryCatch);
 
 	// What is the return value when there was an exception?
 	returnValue = napi_call_function(env, napi_get_global_scope(env),
 		jsFunction, 0, 0);
 
-	napi_set_return_value(env, info, napi_trycatch_exception(env, tryCatch));
-
-	napi_trycatch_delete(env, tryCatch);
+	napi_set_return_value(env, info, napi_get_and_clear_last_exception(env));
 }
 
 NAPI_METHOD(allowException) {
-	napi_trycatch tryCatch;
 	napi_value returnValue;
 	napi_value jsFunction;
 
 	napi_get_cb_args(env, info, &jsFunction, 1);
 
-	napi_trycatch_new(env, &tryCatch);
-
 	returnValue = napi_call_function(env, napi_get_global_scope(env),
 		jsFunction, 0, 0);
-
-	napi_trycatch_delete(env, tryCatch);
 }
 
 NAPI_MODULE_INIT(Init) {
