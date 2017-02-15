490d42e56c6a385e4837d2a778dc4e8fce69b393
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-02-15 13:36:49 +0200

    napi: synchronize function call context test
    Closes https://github.com/nodejs/abi-stable-node/pull/107

--- test/addons-abi/3_callbacks/binding.cc
+++ test/addons-abi/3_callbacks/binding.cc
@@ -21,11 +21,30 @@ void RunCallback(napi_env env, const napi_callback_info info) {
   if (status != napi_ok) return;
 }
 
+void RunCallbackWithRecv(napi_env env, const napi_callback_info info) {
+  napi_status status;
+
+  napi_value args[2];
+  status = napi_get_cb_args(env, info, args, 2);
+  if (status != napi_ok) return;
+
+  napi_value cb = args[0];
+  napi_value recv = args[1];
+
+  status = napi_call_function(env, recv, cb, 0, nullptr, nullptr);
+  if (status != napi_ok) return;
+}
+
 void Init(napi_env env, napi_value exports, napi_value module) {
   napi_status status;
-  napi_property_descriptor desc = { "exports", RunCallback };
-  status = napi_define_property(env, module, &desc);
-  if (status != napi_ok) return;
+  napi_property_descriptor desc[2] = {
+    { "RunCallback", RunCallback },
+    { "RunCallbackWithRecv", RunCallbackWithRecv }
+  };
+  for (int index = 0; index < 2; index++) {
+    status = napi_define_property(env, exports, &desc[index]);
+    if (status != napi_ok) return;
+  }
 }
 
 NODE_MODULE_ABI(addon, Init)
