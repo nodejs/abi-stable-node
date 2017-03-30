b0ddf6ae008e8f151388a434e4e95c73cc5b2856
Sampson Gao <sampsong@ca.ibm.com>
2017-03-30 17:31:57 -0400

    Add test for string apis

--- test/addons-napi/test_string/test.js
+++ test/addons-napi/test_string/test.js
@@ -6,21 +6,28 @@ const assert = require('assert');
 const test_string = require(`./build/${common.buildType}/test_string`);
 
 const str1 = 'hello world';
-assert.strictEqual(test_string.Copy(str1), str1);
+assert.strictEqual(test_string.TestLatin1(str1), str1);
+assert.strictEqual(test_string.TestUtf8(str1), str1);
+assert.strictEqual(test_string.TestUtf16(str1), str1);
 assert.strictEqual(test_string.Length(str1), 11);
 assert.strictEqual(test_string.Utf8Length(str1), 11);
 
 const str2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-assert.strictEqual(test_string.Copy(str2), str2);
+assert.strictEqual(test_string.TestLatin1(str2), str2);
+assert.strictEqual(test_string.TestUtf8(str2), str2);
+assert.strictEqual(test_string.TestUtf16(str2), str2);
 assert.strictEqual(test_string.Length(str2), 62);
 assert.strictEqual(test_string.Utf8Length(str2), 62);
 
 const str3 = '?!@#$%^&*()_+-=[]{}/.,<>\'"\\';
-assert.strictEqual(test_string.Copy(str3), str3);
+assert.strictEqual(test_string.TestLatin1(str3), str3);
+assert.strictEqual(test_string.TestUtf8(str3), str3);
+assert.strictEqual(test_string.TestUtf16(str3), str3);
 assert.strictEqual(test_string.Length(str3), 27);
 assert.strictEqual(test_string.Utf8Length(str3), 27);
 
 const str4 = '\u{2003}\u{2101}\u{2001}';
-assert.strictEqual(test_string.Copy(str4), str4);
+assert.strictEqual(test_string.TestUtf8(str4), str4);
+assert.strictEqual(test_string.TestUtf16(str4), str4);
 assert.strictEqual(test_string.Length(str4), 3);
 assert.strictEqual(test_string.Utf8Length(str4), 9);
